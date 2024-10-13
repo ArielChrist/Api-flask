68836bc64492893bb49d5cc5f77a048c6c882cd4
Ariel Christ NGATO <ArielChrist>
2024-10-13 18:16:43 +0000

    changement dans model

--- app/models.py
+++ app/models.py
@@ -34,7 +34,7 @@ class Post(db.Model):
     title = db.Column(db.String(200), nullable=False)
     content = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    author_id = db.Column(db.Integer, db.ForeignKey('author.id'), nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     author = db.relationship('User', back_populates='posts')
     tags = db.relationship('Tag', secondary=post_tags, back_populates='posts')
 
